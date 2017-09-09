1702d8b35159c6f7bd8a7df64337f6f215d80d1f
Matheus Barone <matheusvbarone@gmail.com>
2017-09-09 12:15:25 -0300

    Criando a função de validação do número de caracteres do input

--- cep.js
+++ cep.js
@@ -3,15 +3,21 @@ $(document).ready(()=>{
   const validateEntry = function(e){
       //console.log(e.target.value);
       this.value = this.value.replace(/\D/g,"");
+      this.value = this.value.replace(/^(\d{5})(\d)/,"$1-$2");
   }
 
-  const getAddress = ()=> {
-
+  const validateLength = function() {
+      let cep = this.value;
+      if(cep.length < 8){
+        $(this).addClass("error").focus();
+      }else{
+        $(this).removeClass("error");
+      }
   }
 
   //MAPEANDO EVENTOS
   $("#cep")
     .on("input", validateEntry)
-    .on("focusout", getAddress);
+    .on("focusout", validateLength);
 
 });
