717e3e4aadafc1e401990294ae485ff6ff6507f8
Matheus Barone <matheusvbarone@gmail.com>
2017-09-09 12:50:46 -0300

    Fazendo a requisicao ajax

--- cep.js
+++ cep.js
@@ -3,7 +3,18 @@ $(document).ready(()=>{
   const validateEntry = function(e){
       //console.log(e.target.value);
       this.value = this.value.replace(/\D/g,"");
-      this.value = this.value.replace(/^(\d{5})(\d)/,"$1-$2");
+      // this.value = this.value.replace(/^(\d{5})(\d)/,"$1-$2");
+  }
+
+  const getAddress = function(cep){
+      $.ajax({
+          url:`https://viacep.com.br/ws/${cep}/json/`,
+          dataType: "json",
+          error: getAddressError,
+          success: getAddressSuccess
+          // beforeSend,
+          // complete
+      });
   }
 
   const validateLength = function() {
@@ -12,9 +23,17 @@ $(document).ready(()=>{
         $(this).addClass("error").focus();
       }else{
         $(this).removeClass("error");
+        getAddress(cep);
       }
   }
 
+const getAddressSuccess = function(address){
+  console.log("Sucesso");
+}
+
+const getAddressError = function(){
+  console.log("O servidor retornou com um erro");
+}
   //MAPEANDO EVENTOS
   $("#cep")
     .on("input", validateEntry)
